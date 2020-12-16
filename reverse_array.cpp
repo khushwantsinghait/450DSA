5280b990fab90475fad8067c5842661d9143a65a
Khushwant Singh <50532309+khushwantsinghait@users.noreply.github.com>
2020-12-16 07:22:57 +0530

    Reverse a String Solution

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -1,1 +1,25 @@
-Hemlo
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWord(string str);
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+}
+string reverseWord(string str){
+   string rev=str;
+   int n=str.length();
+   for(int i=0;i<n;i++)
+   {
+       rev[i]=str[n-i-1];
+   }
+   return rev;
+}
